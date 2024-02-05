b243cae8a3ae1283b842f0f6335978eafa4d884c
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-02-05 17:16:25 -0500

    Add third test case

--- test/sum-test.js
+++ test/sum-test.js
@@ -11,4 +11,9 @@ describe('sum', () => {
     const result = sum(-1,-2)
     expect(result).to.equal(-3, '-1 + (-2) should equal -3')
   })
+
+  it('should correctly add a positive and a negative number', () => {
+    const result = sum(-1, 2);
+    expect(result).to.equal(1, '-1 + 2 should equal 1')
+  })
 })
