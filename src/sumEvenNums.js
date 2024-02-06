c32e65eec93dca4e3c796d840111382af66dcc99
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-02-06 17:32:04 -0500

    Add fn and test for sumEvenNums

--- src/sumEvenNums.js
+++ src/sumEvenNums.js
@@ -1,5 +1,11 @@
-function sumEvenNums() {
-
+function sumEvenNums(nums) {
+  let counter = 0
+  for(let i = 0; i < nums.length; i++) {
+    if(nums[i] % 2 === 0) {
+      counter += nums[i]
+    }
+  }
+  return counter
 }
 
 export default sumEvenNums
@@ -8,22 +14,9 @@ export default sumEvenNums
 Objective:
 Write a function sumEvenNums that takes a list of integers and returns the sum of all the even numbers in the list.
 
-Requirements:
-
-The function should iterate through the list and add up all the even numbers.
-If the list contains no even numbers, the function should return 0.
-If the list is empty, the function should also return 0.
-Example Inputs & Outputs:
-
 Input: [1, 2, 3, 4, 5, 6]
 Output: 12 (because 2 + 4 + 6 = 12)
 
 Input: [17, 23, 81]
 Output: 0 (because there are no even numbers)
-
-Input: []
-Output: 0 (because the list is empty)
-
-
-
 */
--- test/sumEvenNums-test.js
+++ test/sumEvenNums-test.js
@@ -1,9 +1,14 @@
-import sumEvenNums from "../src/sumEvenNums";
+import sumEvenNums from "../src/sumEvenNums.js";
 import { expect } from 'chai';
 
-describe('sum even numbers', () => {
+describe('sumEvenNums', () => {
   it('should add only even numbers', () => {
     const result = sumEvenNums([1, 2, 3, 4, 5, 6])
     expect(result).to.equal(12)
+  })
+
+  it('should return 0 since there are no even numbers', () => {
+    const result = sumEvenNums([17, 23, 81])
+    expect(result).to.equal(0)
   })
 })
