21aaca467e9f54eea2fee54986f60540878d49f5
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-02-20 14:35:15 -0500

    Update test to count space

--- test/countCharacterInstances-test.js
+++ test/countCharacterInstances-test.js
@@ -3,7 +3,7 @@ import { expect } from "chai";
 
 describe('countCharacterInstances', () => {
   it('should return an object with the count of each letter', () => {
-    const result = countCharacterInstances("helloworld")
+    const result = countCharacterInstances("hello world")
     expect(result).to.deep.equal({
       h: 1,
       e: 1,
@@ -11,7 +11,8 @@ describe('countCharacterInstances', () => {
       o: 2,
       w: 1,
       r: 1,
-      d: 1
+      d: 1,
+      space: 1
     })
   })
   })
