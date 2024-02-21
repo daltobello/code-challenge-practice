6e19de071146b1e04431681a9cbfa96c19034f78
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-02-21 18:22:30 -0500

    Add test case for case sensitivity

--- test/countCharacterInstances-test.js
+++ test/countCharacterInstances-test.js
@@ -2,7 +2,7 @@ import countCharacterInstances from "../src/countCharacterInstances.js"
 import { expect } from "chai";
 
 describe('countCharacterInstances', () => {
-  it('should return an object with the count of each letter', () => {
+  it('should return an object with the count of each letter in "hello world"', () => {
     const result = countCharacterInstances("hello world")
     expect(result).to.deep.equal({
       h: 1,
@@ -13,6 +13,21 @@ describe('countCharacterInstances', () => {
       r: 1,
       d: 1,
       space: 1
+    })
+  })
+
+  it('should return an object with the count of each letter in "New York dork"', () => {
+    const result = countCharacterInstances("New York dork")
+    expect(result).to.deep.equal({
+      n: 1,
+      e: 1,
+      w: 1,
+      y: 1,
+      d: 1,
+      o: 2,
+      r: 2,
+      k: 2,
+      space: 2
     })
   })
   })
