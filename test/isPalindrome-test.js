9313ea229e03054408c16887c8bfed714105fb48
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-02-22 16:50:13 -0500

    Add passing test cases

--- test/isPalindrome-test.js
+++ test/isPalindrome-test.js
@@ -2,8 +2,13 @@ import isPalindrome from "../src/isPalindrome.js";
 import { expect } from "chai"
 
 describe('isPalindrome', () => {
-  it("should return true if input string is a palindrome and false if not", () => {
+  it("should return true since input string is a palindrome", () => {
     const result = isPalindrome("racecar")
     expect(result).to.equal(true)
+  }) 
+
+  it("should return false since input string is not a palindrome", () => {
+    const result = isPalindrome("smorgasbord")
+    expect(result).to.equal(false)
   }) 
 })
