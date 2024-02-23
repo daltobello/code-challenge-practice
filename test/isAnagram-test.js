08ce6cc8599ebac61346a186f847d1cf79a67f17
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-02-23 18:17:41 -0500

    Add isAnagram test case for anagram strings

---
+++ test/isAnagram-test.js
@@ -0,0 +1,8 @@
+import isAnagram from "../src/isAnagram.js";
+import { expect } from "chai";
+
+describe("isAnagram",() => {
+it("should return true when two input strings are anagrams of each other.", () => {
+  result = isAnagram("earth", "heart")
+})
+})
