67195a4877e270ec8562533952ee7e3a7b4e7a1a
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-02-23 19:42:17 -0500

    Add passing test for anagram strings

--- test/isAnagram-test.js
+++ test/isAnagram-test.js
@@ -1,8 +1,9 @@
 import isAnagram from "../src/isAnagram.js";
 import { expect } from "chai";
 
-describe("isAnagram",() => {
-it("should return true when two input strings are anagrams of each other.", () => {
-  result = isAnagram("earth", "heart")
-})
-})
+describe("isAnagram", () => {
+  it("should return true when two input strings are anagrams of each other.", () => {
+    const result = isAnagram("earth", "heart");
+    expect(result).to.equal(true);
+  });
+});
