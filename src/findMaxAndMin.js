1b97b0326c9267a215fb88dcc2624bf019cbec62
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-02-25 19:10:26 -0500

    Add findMaxAndMix fn skeleton

---
+++ src/findMaxAndMin.js
@@ -0,0 +1,22 @@
+function findMaxAndMin(nums) {
+  let max
+  let min
+
+  
+}
+
+export default findMaxAndMin
+
+/**
+ Given an array of integers, find the maximum and minimum elements in the array.
+Input: [3, 2, 6, 11, 24]
+Output: [2, 24]
+
+GOAL: find the min and max numbers in an array and return them in a new array
+INPUT: array of integers
+OUTPUT: array of two integers
+HOW: 
+- declare max and min variables
+- set the first element of the array to the min
+- loop over the nums array and check if each element is greater than 
+ */
