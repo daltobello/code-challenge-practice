e53d154f238a75eaacb2dcc95d1b112d130a23d9
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-04-07 20:07:47 -0400

    Add fibinocci function pseudocode

---
+++ src/fibonacci.js
@@ -0,0 +1,21 @@
+function fibonacci(n) {
+  
+}
+
+export default fibonacci
+
+/*
+Write a function that takes a number n as input and returns the nth number in the Fibonacci sequence. The Fibonacci sequence is a series of numbers where each number is the sum of the two preceding ones, typically starting with 0 and 1. For example, the sequence goes: 0, 1, 1, 2, 3, 5, 8, 13, 21, and so on.
+
+Your function should adhere to the following requirements:
+
+It should take a single parameter n, which represents the position of the number in the Fibonacci sequence (starting from 0).
+It should return the nth number in the Fibonacci sequence.
+
+GOAL: return the value of the Fibonacci sequence at position n (the number that is input into the function)
+INPUT: number
+OUTPUT: number
+HOW: 
+- 
+
+*/
