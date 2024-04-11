7e384c2ef868f49421b20c96b96b1822bf5ab4b7
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-04-10 21:08:44 -0400

    Add skip to fibonacci test cases

--- test/fibonacci-test.js
+++ test/fibonacci-test.js
@@ -2,12 +2,12 @@ import fibonacci from "../src/fibonacci.js";
 import { expect } from "chai";
 
 describe('fibonacci', () => {
-  it('should returns the 0th number in the Fibonacci sequence', () => {
+  it.skip('should returns the 0th number in the Fibonacci sequence', () => {
     const result = fibonacci(0)
     expect(result).to.equal(0)
   })
 
-  it('should returns the 4th number in the Fibonacci sequence', () => {
+  it.skip('should returns the 4th number in the Fibonacci sequence', () => {
     const result = fibonacci(4)
     expect(result).to.equal(3)
   })
