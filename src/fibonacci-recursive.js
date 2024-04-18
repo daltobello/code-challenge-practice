391dfc9e10ac14e779be68b8109f9cb7beb4f9bb
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-04-18 08:54:09 -0400

    Add fibonacciRecursive function skeleton

---
+++ src/fibonacci-recursive.js
@@ -0,0 +1,15 @@
+function fibonacciRecursive(n) {
+
+}
+
+export default fibonacciRecursive
+
+/*
+recursive approach: 
+- establish the base case
+- keep track of the previous two numbers 
+- call the function inside of itself
+
+
+
+*/
