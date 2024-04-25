410cc606d410cfaac629b926dde4feb23ce8d374
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-04-24 21:31:59 -0400

    Add test case for sortBallColor-test

---
+++ test/sortBallColor-test.js
@@ -0,0 +1,51 @@
+import sortBallColor from "../src/sortBallColor.js";
+import { expect } from "chai";
+
+describe("sortBallColor", () => {
+  it("should sort array of balls by color", () => {
+    const result = sortBallColor([
+      { color: "yellow", size: "small" },
+      { color: "purple", size: "small" },
+      { color: "purple", size: "medium" },
+      { color: "gray", size: "large" },
+      { color: "blue", size: "medium" },
+      { color: "yellow", size: "small" },
+      { color: "blue", size: "large" },
+      { color: "blue", size: "medium" },
+      { color: "gray", size: "small" },
+      { color: "yellow", size: "large" },
+      { color: "purple", size: "medium" },
+      { color: "gray", size: "large" },
+      { color: "blue", size: "small" },
+      { color: "gray", size: "medium" },
+      { color: "yellow", size: "large" },
+      { color: "purple", size: "medium" },
+      { color: "blue", size: "small" },
+      { color: "purple", size: "large" },
+      { color: "gray", size: "medium" },
+      { color: "purple", size: "small" },
+    ]);
+    expect(result).to.deep.equal([
+      { color: "yellow", size: "small" },
+      { color: "yellow", size: "small" },
+      { color: "yellow", size: "large" },
+      { color: "yellow", size: "large" },
+      { color: "purple", size: "small" },
+      { color: "purple", size: "medium" },
+      { color: "purple", size: "medium" },
+      { color: "purple", size: "medium" },
+      { color: "purple", size: "large" },
+      { color: "purple", size: "small" },
+      { color: "blue", size: "medium" },
+      { color: "blue", size: "large" },
+      { color: "blue", size: "medium" },
+      { color: "blue", size: "small" },
+      { color: "blue", size: "small" },
+      { color: "gray", size: "large" },
+      { color: "gray", size: "small" },
+      { color: "gray", size: "large" },
+      { color: "gray", size: "medium" },
+      { color: "gray", size: "medium" },
+    ]);
+  });
+});
