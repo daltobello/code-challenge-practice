3f290af62d49fb679cefdd32d2c4169d33e96c58
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-09-08 20:26:39 -0400

    Add test cases

--- test/firstNonRepeatingCharacter-test.js
+++ test/firstNonRepeatingCharacter-test.js
@@ -11,4 +11,19 @@ describe("firstNonRepeatingCharacter", () => {
     const result = firstNonRepeatingCharacter("Mississippi")
     expect(result).to.equal("M")
   })
+
+  it("should return null when there is no non-repeating character", () => {
+    const result = firstNonRepeatingCharacter("noon")
+    expect(result).to.equal(null)
+  })
+
+  it("should return null when there the input is an empty string", () => {
+    const result = firstNonRepeatingCharacter("")
+    expect(result).to.equal(null)
+  })
+
+  it("should return the only character when the input is one letter", () => {
+    const result = firstNonRepeatingCharacter("b")
+    expect(result).to.equal("b")
+  })
 })
